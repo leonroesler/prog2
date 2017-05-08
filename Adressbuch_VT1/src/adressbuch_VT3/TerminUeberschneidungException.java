48176765433d13f976b2d4e01022b6ca180cbbb6
nixdix <le.on@outlook.com>
2017-05-08 23:52:07 +0200

    UE3 init

---
+++ Adressbuch_VT1/src/adressbuch_VT3/TerminUeberschneidungException.java
@@ -0,0 +1,15 @@
+package adressbuch_VT3;
+
+/**
+ * Zeigt eine Terminueberschneidung an.
+ * @author beuth
+ */
+public class TerminUeberschneidungException extends UngueltigerTerminException {
+    public TerminUeberschneidungException(Termin termin){
+        super(termin);
+    }
+    
+    public String toString(){
+        return String.format("Es gibt bereits einen Termin am %s von %s bis %s%n", termin.getDatum(), termin.getVon(), termin.getBis());
+    }
+}

1d08de62132d6d2bf8443eb97f02b2a9d9583bbb
nixdix <le.on@outlook.com>
2017-05-16 23:20:33 +0200

    leere Tests angelegt

---
+++ Adressbuch_VT1/src/adressbuch_VT3/TerminVerwaltungTest.java
@@ -0,0 +1,51 @@
+package adressbuch_VT3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by leon on 15.05.2017.
+ */
+class TerminVerwaltungTest {
+    @Test
+    void initialisieren() {
+
+    }
+
+    @Test
+    void getTermineTag() {
+
+    }
+
+    @Test
+    void addTermin() {
+
+    }
+
+    @Test
+    void updateTermin() {
+
+    }
+
+    @Test
+    void removeTermin() {
+
+    }
+
+    @Test
+    void checkTerminUeberschneidung() {
+
+    }
+
+    @Test
+    void getTermin() {
+
+    }
+
+    @Test
+    void getAllTermine() {
+
+    }
+
+}

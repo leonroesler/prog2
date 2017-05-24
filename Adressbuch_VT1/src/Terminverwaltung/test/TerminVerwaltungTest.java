b887b80056b6500c5b51d2a997f72aed5895ff0e
KingLeonidas90 <marlon.mattern5@googlemail.com>
2017-05-24 11:16:33 +0200

    Übung 4 Init

--- Adressbuch_VT1/src/Terminverwaltung/test/TerminVerwaltungTest.java
+++ Adressbuch_VT1/src/Terminverwaltung/test/TerminVerwaltungTest.java
@@ -3,10 +3,13 @@ package Terminverwaltung.test;
 
 import Terminverwaltung.src.terminverwaltung.Termin;
 import Terminverwaltung.src.terminverwaltung.TerminUeberschneidungException;
+import Terminverwaltung.src.terminverwaltung.TerminVerwaltung;
+import Terminverwaltung.src.terminverwaltung.UngueltigerTerminException;
 import org.junit.Test;
-
+import org.junit.Before;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.TreeMap;
@@ -16,27 +19,43 @@ import java.util.TreeMap;
  */
 
 public class TerminVerwaltungTest {
-    private TreeMap<LocalDate, ArrayList<Termin>> termineDate;
-    private HashMap<String, Termin> termineId;
 
+    // Standardkonstruktor
+    public TerminVerwaltungTest(){
 
-    @Test
-    public void initialisieren() throws Exception {
     }
+
+
+
+    private TerminVerwaltung verwaltung;
+    private Termin t1;
+    private Termin t2;
+    private Termin t3;
+
+    @Before
+    public void setUp() throws Exception {
+        LocalDate date = LocalDate.of(2014, 5, 19);
+        verwaltung = new TerminVerwaltung();
+        t1 = new Termin("Termin1", date, LocalTime.of(9, 0), LocalTime.of(10, 0));
+        t2 = new Termin("Termin2", date, LocalTime.of(10, 30), LocalTime.of(11, 0));
+        t3 = new Termin("Termin3", date, LocalTime.of(11, 15), LocalTime.of(12, 30));
+        verwaltung.addTermin(t1);
+        verwaltung.addTermin(t2);
+        verwaltung.addTermin(t3);
+    }
+
 
     @Test
     public void getTermineTag() throws Exception {
     }
 
+
+
+
+
     @Test
-    public void addTermin(Termin t) throws Exception {
-        Termin alt = checkTerminUeberschneidung(t);
-        if (alt == null) {
-            //addTerminDate(t);
-            termineId.put(t.getId(), t);
-        } else {
-            throw new TerminUeberschneidungException(alt);
-        }
+    public void addTermin() throws Exception {
+
     }
 
     @Test
@@ -47,9 +66,17 @@ public class TerminVerwaltungTest {
     public void removeTermin() throws Exception {
     }
 
-    @Test
-    public Termin checkTerminUeberschneidung(Termin neu) throws Exception {
-        return neu;
+    @Test(expected = TerminUeberschneidungException.class)
+    public void checkTerminUeberschneidung() throws Exception {
+        Termin t = t1.getCopy();
+        try {
+            t.setVonBis(t2.getVon(), t3.getBis());
+        } catch (UngueltigerTerminException e) {
+            e.printStackTrace();
+        }
+        termineAusgeben(verwaltung.getAllTermine());
+        System.out.println(verwaltung.updateTermin(t));
+
     }
 
     @Test
@@ -59,5 +86,17 @@ public class TerminVerwaltungTest {
     @Test
     public void getAllTermine() throws Exception {
     }
+
+    private void termineAusgeben(Termin[] termine) {
+        if (termine == null || termine.length == 0) {
+            System.out.println("Keine Termine\n");
+        } else {
+            for (Termin t : termine) {
+                System.out.println(t.getInfo());
+                System.out.println();
+            }
+        }
+    }
+
 
 }
